1dc60a3c7ec467b3245586368ef912aba8a8ff63
khamisially <90456146+khamisially@users.noreply.github.com>
2022-01-28 16:29:44 +0300

    Add files via upload

---
+++ Amisa_2022_01_28_13_04_54/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+  createCanvas(350, 400);
+}
+
+function draw() {
+  background(200,0,100);
+  
+  line(0,50,400,300);
+  rectMode(CENTER);
+  rect(200,150,150,150);
+  
+}
---
+++ Exuberant_snowboard_2022_01_28_12_35_51/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(400, 300);
+  background(0);
+}
+
+function draw() {
+  
+  noStroke();
+  fill(255,300,10);
+  circle(mouseX,mouseY,50);
+}
+  function mousePressed() {  
+  background(0);
+}
