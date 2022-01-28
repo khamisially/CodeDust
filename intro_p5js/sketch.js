0fd74bd8c1d5466e42c513c05ec7de1e428c8403
khamisially <90456146+khamisially@users.noreply.github.com>
2022-01-28 15:24:35 +0300

    Add files via upload

---
+++ intro_p5js/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(400, 400);
+}
+//when you are adding numbers the color of//the shape will change
+//then adding the shape on background we can put//rect square acquired then changing numbers the shape will 
+//change the positision
+
+function draw() {
+  background(220,0,100);
+  rect(200,0,150);
+}
